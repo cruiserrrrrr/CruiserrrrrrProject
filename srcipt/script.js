d955f667a3c930495e0659f0e2c7f0c831a0dc85
cruiserrrrrr <pon4ik.44rus@gmail.com>
2022-02-26 20:58:21 +0300

    start
    start

---
+++ srcipt/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//       const slider = new ChiefSlider('.slider', {
+//         loop: true
+// });
+
+console.clear();
+
+$(".slider").slick({
+    // autoplay: true,
+    dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 2
+});
+
+
