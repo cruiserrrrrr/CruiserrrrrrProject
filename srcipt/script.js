ff9adabf8b6af58c9f426181f325cd104a4e2e82
cruiserrrrrr <90287011+cruiserrrrrr@users.noreply.github.com>
2022-02-27 03:12:18 +0300

    add
    add

--- srcipt/script.js
+++ srcipt/script.js
@@ -1,15 +1,42 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//       const slider = new ChiefSlider('.slider', {
-//         loop: true
-// });
-
-console.clear();
-
-$(".slider").slick({
-    // autoplay: true,
-    dots: true,
-  slidesToShow: 3,
-  slidesToScroll: 2
-});
 
 
+// 1 item
+    document.getElementById('base__btn').onclick = function() {
+    document.getElementById('skills__item__1').classList.add('base__act');
+    document.getElementById('closed__btn__1').classList.add('skills__item__1c');
+    }
+
+
+    document.getElementById('closed__btn__1').onclick = function() {
+    document.getElementById('skills__item__1').classList.remove("base__act");
+    document.getElementById('closed__btn__1').classList.remove('skills__item__1c');
+
+    }
+
+   
+// 2 item
+    document.getElementById('comm__btn').onclick = function() {
+    document.getElementById('skills__item__2').classList.add('comm__act');
+    document.getElementById('closed__btn__2').classList.add('skills__item__2c');
+    }
+
+
+    document.getElementById('closed__btn__2').onclick = function() {
+    document.getElementById('skills__item__2').classList.remove("comm__act");
+    document.getElementById('closed__btn__2').classList.remove('skills__item__2c');
+
+    }
+
+
+// 3 item
+    document.getElementById('exp__btn').onclick = function() {
+    document.getElementById('skills__item__3').classList.add('exp__act');
+    document.getElementById('closed__btn__3').classList.add('skills__item__3c');
+    }
+
+
+    document.getElementById('closed__btn__3').onclick = function() {
+    document.getElementById('skills__item__3').classList.remove("exp__act");
+    document.getElementById('closed__btn__3').classList.remove('skills__item__3c');
+
+    }
